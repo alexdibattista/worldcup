74b4748dcb2a435c4705b55997981cf2ea5abe41
Alexander Di Battista <adibattista@nascentdigital.com>
2014-07-02 23:20:46 -0400

    created end points for matches, clubs and playerSeasonStats

--- routes/team.js
+++ routes/team.js
@@ -1,11 +1,13 @@
 /* GET home page. */
+var qs = require('querystring');
 var wcAPI = require('kimono-worldcup-api-wrapper');
 var wcClient = wcAPI('7d6b9fe6904c1788053de874a26e2b42');
 
 exports.all = function(req, res){
 
-
-  wcClient.get.teams().done(function (teams) {
+  var querystring = qs.parse(req.url.split('?')[1]);
+  
+  wcClient.get.teams(querystring).done(function (teams) {
     var teams = JSON.parse(teams[0].body);
     res.render('team', { title: 'Teams', teams: teams });
   });
